1c2e1bdabc7195f71ff2b9671b730a8794a153fa
jinwoo fung <30623585+jnwof@users.noreply.github.com>
2024-08-03 09:26:24 -0400

    Added fingers

--- image-recognition.py
+++ image-recognition.py
@@ -1,43 +1,68 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import tensorflow as tf
+import pathlib
 from tensorflow import keras
 print("TensorFlow version:", tf.__version__)
 
-# Call a load function to store data in tuples
-# x = images, y = labels
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+# Set dimensions for images
+img_height = 128
+img_width = 128
 
-# class names list to visualize 16 images out of the total dataset
-# assign numerical labels with actual keywords
-class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck'] 
+# Loading datasets for finger detection
+train_ds = keras.preprocessing.image_dataset_from_directory(
+    'hands',
+    "HandsTraining",
+    validation_split=0.2,
+    labels="inferred",
+    label_mode="int",
+    image_size=(img_height, img_width),
+    shuffle=True,
+    seed=123,
+    subset="training"
+)
+
+val_ds = keras.preprocessing.image_dataset_from_directory(
+    'hands',
+    "HandsValidation",
+    validation_split=0.2,
+    labels="inferred",
+    label_mode="int",
+    image_size=(img_height, img_width),
+    shuffle=True,
+    seed=123,
+    subset="validation"
+)
+
+class_names = train_ds.class_names
+print(class_names)
+
+data_iterator = train_ds.as_numpy_iterator()
+
 
 # Scale pixel values from (0 ~ 225) --> (0 ~ 1)
-x_train = x_train / 255
-x_test =  x_test / 255
 
 
 # Training the Model
 
 # Reducing the # of images fed to the model - reduces time and saves resources
-x_train = x_train[:1000]
-y_train = y_train[:1000]
-x_test = x_test[:2000]
-y_test = y_test[:2000]
 
-#defining the neural network 
 
-# 32 neurons, (3,3) convolution matrix filter, activation function off relu, 32x32 res with 3 color channels
-# A convolution layer filters image for prominent features
+# Defining the Neural Network
 
+# Using the sequential model - process layers one by one
 model = keras.models.Sequential([
+    #Conv2D extracts detects the many different features of an image (Ex: edges, patterns, textures)
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    # MaxPooling2D extracts the most prominent parts and dampens the less important parts, reducing computational load and the 
+    # image in question to be more obvious. 
     keras.layers.MaxPooling2D((2, 2)),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
     keras.layers.MaxPooling2D((2, 2)),
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
     
+    # Flattening - reshaping a multi-dimensional array into a one-dimensional array
     keras.layers.Flatten(),
     keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
@@ -45,6 +70,7 @@ model = keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='SparseCategoricalCrossentropy', metrics=['accuracy'])
 
+"""
 model.fit(x_train, y_train, epochs=10)
 
 # Checking the model's performance
@@ -55,3 +81,4 @@ print(f"Accuracy: {accuracy}")
 
 #
 model.save('image_classifier.keras')
+"""
