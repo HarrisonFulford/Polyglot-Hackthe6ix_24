3e1ad462cc69a2116780640ccd64ca56c4626c4e
jinwoo fung <30623585+jnwof@users.noreply.github.com>
2024-08-03 10:37:11 -0400

    Applied training

--- image-recognition.py
+++ image-recognition.py
@@ -11,74 +11,50 @@ img_height = 128
 img_width = 128
 
 # Loading datasets for finger detection
-train_ds = keras.preprocessing.image_dataset_from_directory(
-    'hands',
-    "HandsTraining",
-    validation_split=0.2,
-    labels="inferred",
-    label_mode="int",
-    image_size=(img_height, img_width),
-    shuffle=True,
-    seed=123,
-    subset="training"
-)
+ds = keras.utils.image_dataset_from_directory('hands')
 
-val_ds = keras.preprocessing.image_dataset_from_directory(
-    'hands',
-    "HandsValidation",
-    validation_split=0.2,
-    labels="inferred",
-    label_mode="int",
-    image_size=(img_height, img_width),
-    shuffle=True,
-    seed=123,
-    subset="validation"
-)
+class_names = ds.class_names
 
-class_names = train_ds.class_names
-print(class_names)
-
-data_iterator = train_ds.as_numpy_iterator()
-
+data_iterator = ds.as_numpy_iterator()
+batch = data_iterator.next()
 
 # Scale pixel values from (0 ~ 225) --> (0 ~ 1)
+ds = ds.map(lambda x, y: (x/255, y))
 
+# Splitting the Dataset into Training / Validation / Testing Purposes
+train_size = int(len(ds) * 0.7)
+val_size = int(len(ds) * 0.2)
+test_size = int(len(ds) * 0.1) + 1
+
+# Deciding how many batches of the data are allocated for each purpose
+train = ds.take(train_size)
+val = ds.skip(train_size).take(val_size)
+test = ds.skip(train_size + val_size).take(test_size)
 
 # Training the Model
-
-# Reducing the # of images fed to the model - reduces time and saves resources
-
-
 # Defining the Neural Network
 
 # Using the sequential model - process layers one by one
 model = keras.models.Sequential([
     #Conv2D extracts detects the many different features of an image (Ex: edges, patterns, textures)
-    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    # 16 filters, 3x3 pixel filter. Relu converts all negative outputs to 0 and preserves the postive outputs.
+    keras.layers.Conv2D(16, (3, 3), 1, activation='relu', input_shape=(128, 128, 3)), # Input layer
     # MaxPooling2D extracts the most prominent parts and dampens the less important parts, reducing computational load and the 
     # image in question to be more obvious. 
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    keras.layers.MaxPooling2D((2, 2)),
-    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(32, (3, 3), 1, activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(16, (3, 3), 1, activation='relu'),
     
     # Flattening - reshaping a multi-dimensional array into a one-dimensional array
     keras.layers.Flatten(),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
 ])
 
 model.compile(optimizer='adam', loss='SparseCategoricalCrossentropy', metrics=['accuracy'])
+model.summary()
 
-"""
-model.fit(x_train, y_train, epochs=10)
+hist = model.fit(train, epochs=20, validation_data=val)
 
-# Checking the model's performance
-model.evaluate(x_test, y_test)
-loss, accuracy = model.evaluate(x_test, y_test, verbose=2)
-print(f"Loss: {loss}")
-print(f"Accuracy: {accuracy}")
 
-#
-model.save('image_classifier.keras')
-"""
