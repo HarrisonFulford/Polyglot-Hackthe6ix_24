f530f09859707802b9757cc61b1a4ae4e932d665
Nick C <nickc.robotics@gmail.com>
2024-08-03 08:51:36 -0400

    Translation fix

---
+++ PythonTranslation.py
@@ -0,0 +1,18 @@
+languages = ["fr", #French
+        "es", #Spanish
+        "pt", #Portuguese
+        "hi", #Hindi
+        "zh", #Chinese 
+        "ar", #Arabic
+        "bn", #Bengali
+        "jp" #Japanese
+        ]
+languagesIndex = 4
+
+objectName = "Apple"
+to_lang = languages[languagesIndex]
+
+translator= Translator(to_lang)
+#Recive input from object reconision
+translation = translator.translate("Apple")
+print(translation)
